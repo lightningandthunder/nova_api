17d44ca5802200782749b8df8115301d3335032e
lightningandthunder <mike@SWI-LT-201.localdomain>
2019-03-19 22:47:19 -0400

    Testing infrastructure now exists

--- src/dll_tools/tests/fixtures.py
+++ src/dll_tools/tests/fixtures.py
@@ -1,35 +1,69 @@
 from numpy import round
 
-"""Test dictionaries based on Solar Fire output, rounded to 1/10000 of a degree, or 0.36 seconds of arc.
-Intended accuracy for Astronova is approximately 1/1000 of a degree, or 3.6 seconds."""
+"""Test dictionaries based on Solar Fire output."""
 
 
-def compare_charts(chart, fixture):
+def compare_charts(chart, fixture, name):
+    print(f'Testing chart {name}...')
+
+    failed = False
     ecliptic = chart.get_ecliptical_coords()
     for body in ecliptic:
-        assert round(ecliptic[body], decimals=2) == round(fixture['Ecliptical'][body], decimals=2)
+        if not round(ecliptic[body], decimals=2) == round(fixture['Ecliptic'][body], decimals=2):
+            print(f"Test failed on {body} ecliptical coords: {ecliptic[body]} != {fixture['Ecliptic'][body]}")
 
+    if not failed: print('Ecliptical coordinates passed.')
+
+    failed = False
     mundane = chart.get_mundane_coords()
     for body in mundane:
-        assert round(mundane[body], decimals=2) == round(fixture['Mundane'][body], decimals=2)
+        if not round(mundane[body], decimals=0) == round(fixture['Mundane'][body], decimals=0):
+            print(f"Test failed on {body} mundane coords: {mundane[body]} != {fixture['Mundane'][body]}")
+            failed = True
 
+    if not failed: print('Mundane coordinates passed.')
+
+    failed = False
     ra = chart.get_right_ascension_coords()
     for body in ra:
-        assert round(ra[body], decimals=2) == round(fixture['Right Ascension'][body], decimals=2)
+        if not round(ra[body], decimals=2) == round(fixture['Right Ascension'][body], decimals=2):
+            print(f"Test failed on {body} RA coords: {ra[body]} != {fixture['Right Ascension'][body]}")
+            failed = True
 
+    if not failed: print('RA coordinates passed. ')
+
+    failed = False
     cusps = chart.get_cusps_longitude()
     for cusp in cusps:
-        assert round(cusps[cusp], decimals=2) == round(fixture['Cusps'][cusp], decimals=2)
+        if not round(cusps[cusp], decimals=0) == round(fixture['Cusps'][cusp], decimals=0):
+            print(f"Test failed on cusp {cusp}: {cusps[cusp]} != {fixture['Cusps'][cusp]}")
+            failed = True
 
+    if not failed: print('Cusps coordinates passed.')
+
+    failed = False
     angles = chart.get_angles_longitude()
     for angle in angles:
-        assert round(angles[angle], decimals=2) == round(fixture['Angles'][angle], decimals=2)
+        if not round(angles[angle], decimals=0) == round(fixture['Angles'][angle], decimals=0):
+            print(f"Test failed on angle {angle}: {angles[angle]} != {fixture['Angles'][angle]}")
+            failed = True
 
-    assert round(chart.sidereal_framework.LST, decimals=2) == round(fixture['LST'], decimals=2)
-    assert round(chart.sidereal_framework.svp, decimals=2) == round(fixture['SVP'], decimals=2)
-    assert round(chart.sidereal_framework.obliquity, decimals=2) == round(fixture['Obliquity'], decimals=2)
+    if not failed: print('Angle coordinates passed.')
 
-    return True
+    if not round(chart.sidereal_framework.LST, decimals=2) == round(fixture['LST'], decimals=2):
+        print(f"Test failed on LST: {chart.sidereal_framework.LST} != {fixture['LST']}")
+    else:
+        print ('LST passed.')
+
+    if not round(chart.sidereal_framework.svp, decimals=2) == round(fixture['SVP'], decimals=2):
+        print(f"Test failed on SVP: {chart.sidereal_framework.svp} != {fixture['SVP']}")
+    else:
+        print('SVP passed.')
+
+    if not round(chart.sidereal_framework.obliquity, decimals=2) == round(fixture['Obliquity'], decimals=2):
+        print(f"Test failed on obliquity: {chart.sidereal_framework.obliquity} != {fixture['Obliquity']}")
+    else:
+        print('Obliquity passed.')
 
 
 # ==================================================================================================================== #
@@ -37,7 +71,7 @@ def compare_charts(chart, fixture):
 # ==================================================================================================================== #
 
 
-transits_2019_3_18_22_30_15 = {
+transits_2019_3_18_22_30_15_Hackensack = {
     'LST': 9.325,
     'SVP': 4.995,
     'Obliquity': 23.436,
@@ -49,58 +83,126 @@ transits_2019_3_18_22_30_15 = {
         'Mars': 26.9233,
         'Jupiter': 238.5565,
         'Saturn': 264.1137,
-        'Uranus': 4.6030,
+        'Uranus': 5.6030,
         'Neptune': 321.6134,
         'Pluto': 267.8205
     },
     'Mundane': {
-        'Sun': 0,
-        'Moon': 0,
-        'Mercury': 0,
-        'Venus': 0,
-        'Mars': 20,
-        'Jupiter': 0,
-        'Saturn': 0,
-        'Uranus': 0,
-        'Neptune': 0,
-        'Pluto': 0,
+        'Sun': 136.032,
+        'Moon': 284.850,
+        'Mercury': 127.041,
+        'Venus': 93.594,
+        'Mars': 192.304,
+        'Jupiter': 39.346,
+        'Saturn': 62.019,
+        'Uranus': 171.238,
+        'Neptune': 122.649,
+        'Pluto': 65.746,
     },
     'Right Ascension': {
-        'Sun': 0,
-        'Moon': 0,
-        'Mercury': 0,
-        'Venus': 0,
-        'Mars': 20,
-        'Jupiter': 0,
-        'Saturn': 0,
-        'Uranus': 0,
-        'Neptune': 0,
-        'Pluto': 0,
+        'Sun': 358.349,
+        'Moon': 153.658,
+        'Mercury': 350.075,
+        'Venus': 323.221,
+        'Mars': 49.286,
+        'Jupiter': 263.020,
+        'Saturn': 290.633,
+        'Uranus': 28.668,
+        'Neptune': 348.063,
+        'Pluto': 294.681,
     },
     'Cusps': {
-        "1": 0,
-        "2": 0,
-        "3": 0,
-        "4": 0,
-        "5": 0,
-        "6": 0,
-        "7": 0,
-        "8": 0,
-        "9": 0,
-        "10": 0,
-        "11": 0,
-        "12": 0
+        "1": 194.254,
+        "2": 227.347,
+        "3": 261.680,
+        "4": 292.426,
+        "5": 319.359,
+        "6": 345.489,
+        "7": 14.253,
+        "8": 47.347,
+        "9": 81.680,
+        "10": 112.426,
+        "11": 139.359,
+        "12": 165.489
     },
     'Angles': {
-        "Asc": 0,
-        "MC": 0,
-        "Dsc": 0,
-        "IC": 0,
-        "Eq Asc": 0,
-        "Eq Dsc": 0,
-        "EP (Ecliptical)": 0,
-        "Zen": 0,
-        "WP (Ecliptical)": 0,
-        "Ndr": 0
+        "Asc": 194.254,
+        "MC": 112.426,
+        "Dsc": 14.254,
+        "IC": 292.426,
+        "Eq Asc": 207.283,
+        "Eq Dsc": 27.283,
+        "EP (Ecliptical)": 202.349,
+        "Zen": 104.160,
+        "WP (Ecliptical)": 22.349,
+        "Ndr": 284.160
+    }
+}
+
+transits_2019_3_10_1_30_15_Melbourne = {
+    'LST': 9.325,
+    'SVP': 4.995,
+    'Obliquity': 23.436,
+    'Ecliptic': {
+        'Sun': 333.1960,
+        'Moon': 125.5073,
+        'Mercury': 325.4267,
+        'Venus': 295.7339,
+        'Mars': 26.9233,
+        'Jupiter': 238.5565,
+        'Saturn': 264.1137,
+        'Uranus': 5.6030,
+        'Neptune': 321.6134,
+        'Pluto': 267.8205
+    },
+    'Mundane': {
+        'Sun': 136.032,
+        'Moon': 284.850,
+        'Mercury': 127.041,
+        'Venus': 93.594,
+        'Mars': 192.304,
+        'Jupiter': 39.346,
+        'Saturn': 62.019,
+        'Uranus': 171.238,
+        'Neptune': 122.649,
+        'Pluto': 65.746,
+    },
+    'Right Ascension': {
+        'Sun': 358.349,
+        'Moon': 153.658,
+        'Mercury': 350.075,
+        'Venus': 323.221,
+        'Mars': 49.286,
+        'Jupiter': 263.020,
+        'Saturn': 290.633,
+        'Uranus': 28.668,
+        'Neptune': 348.063,
+        'Pluto': 294.681,
+    },
+    'Cusps': {
+        "1": 194.254,
+        "2": 227.347,
+        "3": 261.680,
+        "4": 292.426,
+        "5": 319.359,
+        "6": 345.489,
+        "7": 14.253,
+        "8": 47.347,
+        "9": 81.680,
+        "10": 112.426,
+        "11": 139.359,
+        "12": 165.489
+    },
+    'Angles': {
+        "Asc": 194.254,
+        "MC": 112.426,
+        "Dsc": 14.254,
+        "IC": 292.426,
+        "Eq Asc": 207.283,
+        "Eq Dsc": 27.283,
+        "EP (Ecliptical)": 202.349,
+        "Zen": 104.160,
+        "WP (Ecliptical)": 22.349,
+        "Ndr": 284.160
     }
 }
--- src/dll_tools/tests/test_chartmanager.py
+++ src/dll_tools/tests/test_chartmanager.py
@@ -5,39 +5,29 @@ from timeit import default_timer as timer
 from ChartData import ChartData
 from ChartManager import ChartManager
 from swissephlib import SwissephLib
-from . import fixtures
+from tests import fixtures
 
 swiss_lib = SwissephLib()
 manager = ChartManager(swiss_lib)
 
 # Test plain charts
 
-# 2019/3/18 22:30:15
+# 2019/3/18 22:30:15 Hackensack
 ldt = pendulum.datetime(2019, 3, 18, 22, 30, 15, tz='America/New_York')
 udt = ldt.in_tz('UTC')
 lat = 40.9792
 long = -74.1169
 chart = manager.create_chartdata(ldt, long, lat)
+fixtures.compare_charts(chart, fixtures.transits_2019_3_18_22_30_15_Hackensack, "2019-3-18 22:30:15 NY")
 
-fixtures.transits_2019_3_18_22_30_15['Ecliptic']
+# 2019/3/10 3:30:15am Melbourne
+ldt = pendulum.datetime(2019, 3, 18, 22, 30, 15, tz='Australia/Melbourne')
+udt = ldt.in_tz('UTC')
+lat = 40.9792
+long = -74.1169
+chart = manager.create_chartdata(ldt, long, lat)
+fixtures.compare_charts(chart, fixtures.transits_2019_3_18_22_30_15_Hackensack, "2019-3-18 22:30:15 NY")
 
-print(chart.get_ecliptical_coords())
-print(chart.get_mundane_coords())
-print(chart.get_right_ascension_coords())
-print(chart.sidereal_framework.LST)
-print(chart.sidereal_framework.svp)
-print(chart.sidereal_framework.obliquity)
-print(chart.get_cusps_longitude())
-print(chart.get_angles_longitude())
-
-#
-#
-# name = 'Transits in LA'
-# ldt = pendulum.datetime(2019, 3, 3, 22, 40, 24, tz='America/Los_Angeles')
-# udt = ldt.in_timezone('UTC')
-# lat = 34.0522
-# long = -118.2427
-#
 # la = manager.create_chartdata(name, ldt, long, lat)
 # manager.precess_into_sidereal_framework(radix=mike, transit_chart=la)
 #
@@ -46,19 +36,3 @@ print(chart.get_angles_longitude())
 # long = -66.85
 #
 # test = manager.create_chartdata('', dt, long, lat)
-#
-#
-# name = 'Mike'
-# ldt = pendulum.datetime(1989, 12, 20, 22, 20, 0, tz='America/New_York')
-# udt = ldt.in_tz('UTC')
-# lat = 40.9792
-# long = -74.1169
-# mike = manager.create_chartdata(name, ldt, long, lat)
-#
-# now = pendulum.datetime(2019, 3, 11)
-# start = timer()
-# L = manager.generate_return_list(mike, date=now, body=1, harmonic=4, return_quantity=10)
-# end = timer()
-# print(end - start)
-# for x in L:
-#     print(x.utc_datetime)
